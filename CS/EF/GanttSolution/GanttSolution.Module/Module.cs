cbc505d5f643502df9e952fc79ddc8e47dc65445
Andrey K <andrey.kozhevnikov@devexpress.com>
2025-11-26 12:25:12 +0400

    remove objects

--- CS/EF/GanttSolution/GanttSolution.Module/Module.cs
+++ CS/EF/GanttSolution/GanttSolution.Module/Module.cs
@@ -19,7 +19,6 @@ public sealed class GanttSolutionModule : ModuleBase {
 		// GanttSolutionModule
 		// 
 		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.SystemModule.SystemModule));
-		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule));
     }
     public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
         ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
--- CS/XPO/GanttSolutionXPO/GanttSolutionXPO.Module/Module.cs
+++ CS/XPO/GanttSolutionXPO/GanttSolutionXPO.Module/Module.cs
@@ -22,7 +22,6 @@ public sealed class GanttSolutionXPOModule : ModuleBase {
 		// GanttSolutionXPOModule
 		// 
 		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.SystemModule.SystemModule));
-		RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule));
     }
     public override IEnumerable<ModuleUpdater> GetModuleUpdaters(IObjectSpace objectSpace, Version versionFromDB) {
         ModuleUpdater updater = new DatabaseUpdate.Updater(objectSpace, versionFromDB);
